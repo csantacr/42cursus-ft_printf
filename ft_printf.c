60e2781f67e60082c3c551aee0c270d485502e44
csantacr <csantacr@student.42.fr>
2023-07-17 16:17:07 +0200

    fixed cc

--- ft_printf.c
+++ ft_printf.c
@@ -14,24 +14,14 @@
 #include "libft.h"
 #include <stdio.h>
 
-/*
-- %c Imprime un solo carácter.
-- %s Imprime una string (como se define por defecto en C).
-• %p El puntero void * dado como argumento se imprime en formato hexadecimal.
-- %d Imprime un número decimal (base 10).
-- %i Imprime un entero en base 10.
-- %u Imprime un número decimal (base 10) sin signo.
-• %x Imprime un número hexadecimal (base 16) en minúsculas.
-• %X Imprime un número hexadecimal (base 16) en mayúsculas.
-- % % para imprimir el símbolo del porcentaje.
-*/
-
-int	print_item(char type, char const *str, va_list ap, int cc)
+int	print_item(char type, const char *str, va_list ap)
 {
+	int	cc;
+	
 	//printf("\n---print_item---\n");
 	//printf("tipo: %c\n", type);
-	if (type == 'l') // l = literal
-		//cc += write(1, str, 1);
+	cc = 0;
+	if (type == 'l')
 		cc += write(1, str, 1);
 	else if (type == 'c')
 		cc += ft_putchar(va_arg(ap, int));
@@ -43,33 +33,35 @@ int	print_item(char type, char const *str, va_list ap, int cc)
 		cc += ft_putunbr(va_arg(ap, unsigned int));
 	else if (type == '%')
 		cc += ft_putchar('%');
+	else if (type == 'x')
+		cc += ft_puthex(va_arg(ap, int), "0123456789abcdef");
+	else if (type == 'X')
+		cc += ft_puthex(va_arg(ap, int), "0123456789ABCDEF");
+	//printf("count: %d\n", cc);
 	//printf("\n///print_item///\n");
 	return (cc);
 }
 
-int	ft_printf(char const *str, ...)
+int	ft_printf(const char *str, ...)
 {
-	int		cc;
+	int		c_count;
 	va_list	ap;
 
-	cc = 0;
+	c_count = 0;
 	va_start(ap, str);
 	while (*str)
 	{
-		if (*str != '%') // si no hay tipo, pasar tipo nulo/literal
+		if (*str != '%')
+			c_count += print_item('l', str, ap);
+		else
 		{
-			print_item('l', str, ap, cc);
-			
-		}
-		else // si hay tipo, print
-		{
-			print_item(*(str + 1), str, ap, cc);
+			c_count += print_item(*(str + 1), str, ap);
 			str++;
 		}
 		str++;
 	}
 	va_end(ap);
-	return (cc);
+	return (c_count);
 }
 
 int main(void)
@@ -78,8 +70,9 @@ int main(void)
 	char c2 = 'g';
 	char *str = "hola";
 	unsigned int un = 4294967294;
+	int max_int = 2147483647;
 
-	printf("ft: %d\n", ft_printf("ab%sc%u%%c%cef%c\n", str, un, c, c2));
-	printf("og: %d\n", printf("ab%sc%u%%c%cef%c\n", str, un, c, c2));
+	printf("ft: %d\n", ft_printf("ab%sc%u%%c%cef%c max_int: %d max_int_hex: %x\n", str, un, c, c2, max_int, max_int));
+	printf("og: %d\n", printf("ab%sc%u%%c%cef%c max_int: %d max_int_hex: %x\n", str, un, c, c2, max_int, max_int));
 	return (0);
 }
