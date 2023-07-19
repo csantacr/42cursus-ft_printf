45347b9899ae66b225fabf7a3e74c3165cc9191b
csantacr <csantacr@student.42madrid.com>
2023-07-19 12:36:49 +0200

    added system null pointer

--- ft_printf_tools/ft_puthexptr.c
+++ ft_printf_tools/ft_puthexptr.c
@@ -6,11 +6,17 @@
 /*   By: csantacr <csantacr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 12:05:44 by csantacr          #+#    #+#             */
-/*   Updated: 2023/07/19 12:11:03 by csantacr         ###   ########.fr       */
+/*   Updated: 2023/07/19 12:36:15 by csantacr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_tools.h"
+
+#ifdef __linux__
+# define NULL_PTR_STR "(nil)"
+#elif __APPLE__
+# define NULL_PTR_STR "0x0"
+#endif
 
 int	ft_puthexptr(unsigned long ptr)
 {
