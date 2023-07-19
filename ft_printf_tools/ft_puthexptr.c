d62aea4b410cf9980468353ce597790445c06da8
csantacr <csantacr@student.42.fr>
2023-07-19 09:26:26 +0200

    all tests passed

--- ft_printf_tools/ft_puthexptr.c
+++ ft_printf_tools/ft_puthexptr.c
@@ -1,14 +1,22 @@
 #include "ft_printf_tools.h"
 
+# ifdef __linux__
+#  define NULL_PTR_STR "(nil)"
+# elif __APPLE__
+#  define NULL_PTR_STR "0x0"
+# endif
+
 int	ft_puthexptr(unsigned long ptr)
 {
 	int		cc;
 
 	cc = 0;
-	cc += ft_putstr("0x");
-	if (ptr == 0)
-		cc += ft_putchar('0');
+	if (!ptr)
+		cc += ft_putstr(NULL_PTR_STR);
 	else
+	{
+		cc += ft_putstr("0x");
 		cc += ft_putlonghex(ptr, "0123456789abcdef");
+	}
 	return (cc);
 }
